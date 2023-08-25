6a6317435ff4bd3d13bd68e354e685ee378bc3f6
dcm323 <danielcalle323@gmail.com>
2023-08-25 11:16:49 -0500

    nt4 se crea inventario de peluche

---
+++ ciclos.py
@@ -0,0 +1,32 @@
+peluches=["roshi", "conejito", "aguacatico"]
+print(peluches)
+
+opcion=0
+print("Pelucheria PELUCHITOS")
+print("....................")
+print("1. Agregar producto ala bodega")
+print("2. Ver productos en bodega")
+print("3. Obtener valor del inventario")
+print("4. Ver productos más vendidos")
+print("5. SALIR")
+
+
+
+while(opcion!=5):
+    opcion=int(input("Digita un número: "))
+    
+    if opcion==1:
+        print("usted esta en la opcion 1")
+    elif opcion==2:
+        print("bro estas en la opcion 2")
+    elif opcion==3:
+        print("bro estas en la opcion 3")
+    elif opcion==4:
+        print("bro estas en la opcion 4")
+    else:
+        print("opcion invalida")   
+    
+print("sali del ciclo")    
+    
+        
+ 
